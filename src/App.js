94998bd87f6f250296b87a23d59e057d765b789b
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2020-11-04 23:46:18 +1100

    created react quiz app

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import questions from "./components/questions";
+import { useState } from "react";
+
+function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore((prev) => prev + 1);
+    }
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+    } else {
+      setShowScore(true);
+    }
+  };
+  return (
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+          {score < questions.length ? <p>Try again</p> : <p>You won!</p>}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button onClick={() => handleClick(answerOption.isCorrect)}>
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
